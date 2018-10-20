3ee8ff1409645e022e5e7af773f833259ef0b5d1
DatGatto <traceformula2000@gmail.com>
2018-10-20 13:26:50 +0700

    UserbasedRecommender | fix syntax error

--- recommender/src/main/java/datgatto/UserbasedRecommender.java
+++ recommender/src/main/java/datgatto/UserbasedRecommender.java
@@ -36,7 +36,7 @@ public class UserbasedRecommender {
         DataModel model = new FileDataModel(new File("input/foody_rate.csv"));
         
         RecommenderBuilder builder = new RecommenderBuilder() {
-            public Recommender buildRecommender(DataModel model) throws TasteException{
+            public Recommender buildRecommender(DataModel dataModel) throws TasteException{
                 UserSimilarity similarity = new PearsonCorrelationSimilarity(dataModel);
                 UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, dataModel);
                 return new GenericUserBasedRecommender(dataModel, neighborhood, similarity);
