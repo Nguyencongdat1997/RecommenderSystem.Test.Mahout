771c6d14f564047c418c08e2d4c22cc186a73655
DatGatto <traceformula2000@gmail.com>
2018-10-20 14:58:14 +0700

    UserbasedRecommender | Update SimilarityMetrics

--- recommender/src/main/java/datgatto/UserbasedRecommender.java
+++ recommender/src/main/java/datgatto/UserbasedRecommender.java
@@ -10,6 +10,9 @@ import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
 import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
 import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
 import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.impl.similarity.EuclideanDistanceSimilarity;
+import org.apache.mahout.cf.taste.impl.similarity.TanimotoCoefficientSimilarity;
+import org.apache.mahout.cf.taste.impl.similarity.LogLikelihoodSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
@@ -35,6 +38,7 @@ public class UserbasedRecommender {
 
         DataModel model = new FileDataModel(new File("input/foody_rate.csv"));
         
+        //PearsonCorrelationSimilarity
         RecommenderBuilder builder = new RecommenderBuilder() {
             public Recommender buildRecommender(DataModel dataModel) throws TasteException{
                 UserSimilarity similarity = new PearsonCorrelationSimilarity(dataModel);
@@ -44,7 +48,48 @@ public class UserbasedRecommender {
         };
 
         RecommenderEvaluator evaluator = new AverageAbsoluteDifferenceRecommenderEvaluator();        
-        double result = evaluator.evaluate(builder, null, model, 0.9, 1.0);
-        System.out.println(result);
+        double result = evaluator.evaluate(builder, null, model, 0.7, 0.2);
+        System.out.println("PearsonCorrelationSimilarity" + result);
+
+        //EuclideanDistanceSimilarity
+        builder = new RecommenderBuilder() {
+            public Recommender buildRecommender(DataModel dataModel) throws TasteException{
+                UserSimilarity similarity = new EuclideanDistanceSimilarity(dataModel);
+                UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, dataModel);
+                return new GenericUserBasedRecommender(dataModel, neighborhood, similarity);
+            }
+        };
+
+        evaluator = new AverageAbsoluteDifferenceRecommenderEvaluator();        
+        double result = evaluator.evaluate(builder, null, model, 0.7, 0.2);
+        System.out.println("EuclideanDistanceSimilarity" + result);
+
+        //TanimotoCoefficientSimilarity
+        builder = new RecommenderBuilder() {
+            public Recommender buildRecommender(DataModel dataModel) throws TasteException{
+                UserSimilarity similarity = new TanimotoCoefficientSimilarity(dataModel);
+                UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, dataModel);
+                return new GenericUserBasedRecommender(dataModel, neighborhood, similarity);
+            }
+        };
+
+        evaluator = new AverageAbsoluteDifferenceRecommenderEvaluator();        
+        double result = evaluator.evaluate(builder, null, model, 0.7, 0.2);
+        System.out.println("TanimotoCoefficientSimilarity" + result);
+
+        //LogLikelihoodSimilarity
+        builder = new RecommenderBuilder() {
+            public Recommender buildRecommender(DataModel dataModel) throws TasteException{
+                UserSimilarity similarity = new LogLikelihoodSimilarity(dataModel);
+                UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, dataModel);
+                return new GenericUserBasedRecommender(dataModel, neighborhood, similarity);
+            }
+        };
+
+        evaluator = new AverageAbsoluteDifferenceRecommenderEvaluator();        
+        double result = evaluator.evaluate(builder, null, model, 0.7, 0.2);
+        System.out.println("LogLikelihoodSimilarity" + result);
+
+
     }
 }
