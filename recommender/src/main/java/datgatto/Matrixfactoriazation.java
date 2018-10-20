a9923cde830b9300cc37ecfced2951171e6b006d
DatGatto <traceformula2000@gmail.com>
2018-10-20 12:07:34 +0700

    Matrixfactorization | Implement

---
+++ recommender/src/main/java/datgatto/Matrixfactoriazation.java
@@ -0,0 +1,62 @@
+package datgatto;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+import org.apache.mahout.cf.taste.eval.RecommenderEvaluator;
+import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
+import org.apache.mahout.cf.taste.eval.RecommenderIRStatsEvaluator;
+import org.apache.mahout.cf.taste.eval.IRStatistics
+import org.apache.mahout.cf.taste.impl.eval.GenericRecommenderIRStatsEvaluator;
+import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommenderEvaluator;
+import org.apache.mahout.cf.taste.recommender.Recommender;
+import org.apache.mahout.cf.taste.eval.DataModelBuilder;
+import org.apache.mahout.cf.taste.impl.common.FastByIDMap;
+import org.apache.mahout.cf.taste.impl.model.GenericDataModel;
+import org.apache.mahout.cf.taste.impl.model.GenericBooleanPrefDataModel;
+import org.apache.mahout.cf.taste.model.PreferenceArray;
+import org.apache.mahout.cf.taste.impl.recommender.svd.SVDRecommender;
+import org.apache.mahout.cf.taste.impl.recommender.svd.ALSWRFactorizer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Matrixfactoriazation {
+
+    public static void main(String[] args) throws IOException, TasteException {
+
+        Logger log = LoggerFactory.getLogger(UserbasedRecommender.class);
+
+        DataModel dataModel = new GenericBooleanPrefDataModel(GenericBooleanPrefDataModel.toDataMap(
+            new FileDataModel(new File("input/foody_rate.csv"))));
+        // DataModelBuilder
+        DataModelBuilder modelBuilder = new DataModelBuilder() {
+            @Override
+            public DataModel buildDataModel(FastByIDMap<PreferenceArray> trainingData) {
+                return new GenericBooleanPrefDataModel(GenericBooleanPrefDataModel.toDataMap(trainingData));
+            }
+        };
+        // RecommenderBuilder
+        RecommenderBuilder recommenderBuilder = new RecommenderBuilder() {
+            @Override
+            public Recommender buildRecommender(DataModel model) throws TasteException {
+                return new SVDRecommender(model, new ALSWRFactorizer(model, 10, 0.065, 10));
+            }
+        };
+        // Evaluator
+        RecommenderIRStatsEvaluator revaluator = new GenericRecommenderIRStatsEvaluator();
+        IRStatistics stats = revaluator.evaluate(recommenderBuilder, modelBuilder, dataModel, null, 10,
+                GenericRecommenderIRStatsEvaluator.CHOOSE_THRESHOLD, 0.7);
+        System.out.println(stats.getPrecision());
+    }
+}
